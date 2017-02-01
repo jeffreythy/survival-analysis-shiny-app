95a2dcdfa08fc058337857521d634df3af1c58ec
jeffreythy <jeffreythy@yahoo.com>
2017-02-01 11:15:27 -0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -9,7 +9,9 @@ shinyUI(navbarPage("Survival Analysis",
   sidebarLayout(
     sidebarPanel(
       h3("Survivial Graph"),
+      # SelectInput gives you the option to choose the variable you want to observe in a dropdown list
       selectInput('sur_var', 'Factor of Survival', names(df)[names(df) != "time" & names(df) != "delta"]),
+      # SliderInput, in this case, let you select the time point you want to observe
       sliderInput('xvalue', 'Survival Days = ',value=100, min=1, max=max(df$time))
       )
     ,
