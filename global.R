27bd4b420555fb72d0fd1482ebb3109d4f4c8088
jeffreythy <jeffreythy@yahoo.com>
2017-01-31 21:07:00 -0800

    Add files via upload

---
+++ global.R
@@ -0,0 +1,6 @@
+library(OIsurv)
+data("tongue")
+tongue$type <- as.factor(tongue$type)
+gender <- sample(c("f","m"), 80, replace=TRUE)
+category <- sample(c("a","b","c"), 80, replace=TRUE)
+df <- as.data.frame(cbind(tongue[1:3], gender, category))
